e2e11e75ab34045dc7cfc280c3bacb8a948c399d
Justin Wang <justinw827@aol.com>
2018-07-19 04:17:05 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,10 @@
 # Write your code here.
 def line(katz_deli)
   if katz_deli.length == 0
-    "The line is currently empty."
+    return "The line is currently empty."
+  else
+    output = ""
+    katz_deli.each_with_index do |name, num|
+      output += (num + '. ' + name + ' ')
+  end
 end
