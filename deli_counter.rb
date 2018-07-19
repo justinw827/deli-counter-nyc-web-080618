5a2b34b1d658ab79fb6f6dd0fe2c989164c40a5b
Justin Wang <justinw827@aol.com>
2018-07-19 04:31:18 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,5 +13,5 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
-  puts "Welcome, #{name}. You are number #{katz_deli.length}"
+  puts "Welcome, #{name}. You are number #{katz_deli.length}."
 end
